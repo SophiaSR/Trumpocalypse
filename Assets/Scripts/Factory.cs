53b0a2a74bf891d6d552a351cb783c1fbb76a77d
sophia <sophia.roshal@gmail.com>
2016-11-27 13:16:12 -0500

    Added application, photos of leaders, score

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class Factory : Singleton<Factory>
 {
     public ArrayList powers = new ArrayList();
+    public Sprite[] images = new Sprite[7];
     public class WorldPower
     {
-        
-        public string leader;
+        public Sprite image;
 
         public string name;
+
+        public string leader;
+
 
         public string q1;
         public string q2;
@@ -21,8 +24,9 @@ public class Factory : Singleton<Factory>
 
         public int score;
         public bool met;
-        public WorldPower(string leader, string q1, string q2, string a1R, string a1W,string a2R, string a2W, int score)
+        public WorldPower(string name, string leader, string q1, string q2, string a1R, string a1W,string a2R, string a2W, int score,Sprite image)
         {
+            this.name = name;
             this.leader = leader;
             this.q1 = q1;
             this.q2 = q2;
@@ -32,6 +36,7 @@ public class Factory : Singleton<Factory>
             this.a2W = a2W;
             this.score = score;
             met = true;
+            this.image = image;
             
         }
 
@@ -41,18 +46,22 @@ public class Factory : Singleton<Factory>
             if(-2 < score && score < 2)
             {
                 score += possibleScore;
+                return possibleScore;
             }
 
             else if(score == 2 && possibleScore < 0)
             {
                 score += possibleScore;
+                return possibleScore;
             }
             else if(score == -2 && possibleScore > 0) 
             {
                 score += possibleScore;
+                return possibleScore;
             }
+            return 0;
 
-            return score;
+            
         }
         
 
@@ -60,25 +69,25 @@ public class Factory : Singleton<Factory>
     }
 
 	void Start() { 
-        WorldPower russia = new WorldPower("President Putin","What is my, the Great Putin, prized color?", "Did we start the Ukrainian conflict?", "Red","Gold", "No","Yes",  2);
+        WorldPower russia = new WorldPower("Russia","President Putin","What is my, the Great Putin, prized color?", "Did we start the Ukrainian conflict?", "Red","Gold", "No","Yes",  2,images[0]);
         powers.Add(russia);
 
-        WorldPower middleEast = new WorldPower("King Abdullah","Is our flag, Saudi Arabia's, blue?", "Are you smart or stupid?", "No","Yes", "Stupid","Smart", -2);
+        WorldPower middleEast = new WorldPower("Middle East","King Abdullah","Is Saudi Arabia's flag blue?", "Are you smart or stupid?", "No","Yes", "Stupid","Smart", -2,images[1]);
         powers.Add(middleEast);
 
-        WorldPower mexico = new WorldPower("President Nieto", "How do you view illegal immigrants?", "Will you tear down the wall?", "As people", "As animals", "Right away","Probably not",   -2);
+        WorldPower mexico = new WorldPower("Mexico","President Nieto", "How do you view illegal immigrants?", "Will you tear down the wall?", "As people", "As animals", "Right away","Probably not",   -2,images[2]);
         powers.Add(mexico);
 
-        WorldPower china = new WorldPower("President Xi Jinping", "How many stars on our flag?", "Are we communist?","Five","Six", "Yes","No", 0);
+        WorldPower china = new WorldPower("China","President Xi Jinping", "How many stars on our flag?", "Are we communist?","Five","Six", "Yes","No", 0,images[3]);
         powers.Add(china);
 
-        WorldPower northKorea = new WorldPower("Leader Kim Jong-un", "What is my height?", "Are we over or under South Korea?", "6","5", "Over","Under",  -1);
+        WorldPower northKorea = new WorldPower("North Korea","Leader Kim Jong-un", "What is my height?", "Are we over or under South Korea?", "6","5", "Over","Under",  -1,images[4]);
         powers.Add(northKorea);
 
-        WorldPower canada = new WorldPower("Prime Minister Trudeau","What topping do you put on your pancakes?", "Are you smart or stupid?","Maple Syrup","Butter", "Smart","Stupid", 0);
+        WorldPower canada = new WorldPower("Canada","Prime Minister Trudeau","What topping do you put on your pancakes?", "Are you smart or stupid?","Maple Syrup","Butter", "Smart","Stupid", 0,images[5]);
         powers.Add(canada);
 
-        WorldPower EU = new WorldPower("President Tusk","What is our currency?", "Was Brexit a good idea?", "Euro","Pound", "No","Yes", 1);
+        WorldPower EU = new WorldPower("European Union","President Tusk","What is our currency?", "Was Brexit a good idea?", "Euro","Pound", "No","Yes", 1,images[6]);
         powers.Add(EU);
 
 
@@ -87,6 +96,10 @@ public class Factory : Singleton<Factory>
     public WorldPower generateAndRemove()
     {
         int randomInt = Random.Range(0, powers.Count);
+        if(powers.Count == 0)
+        {
+            return null;
+        }
         WorldPower wp = powers[randomInt] as WorldPower;
         if (wp.met)
         {
--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -1,58 +1,131 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class OnClick : MonoBehaviour {
+public class OnClick : MonoBehaviour
+{
+    public Sprite endImage;
+    public bool first = true;
+    public int score = -2;
+    Factory.WorldPower wp;
 
-    
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void clicked()
     {
+
+        if (wp != null)
+        {
+            print("hello");
+            GameObject go = EventSystem.current.currentSelectedGameObject;
+
+
+            print("Score: " + score + " wpScore: " + wp.score);
+            if (go.Equals(GameObject.Find("Enter")))
+            {
+                if (GameObject.Find("Feet").GetComponent<Dropdown>().value == 2)
+                {
+                    score += wp.addScore(1);
+                }
+                else
+                {
+                    score += wp.addScore(-1);
+                }
+
+            }
+            else if (go.Equals(GameObject.Find("Correct")))
+            {
+
+                score += wp.addScore(1);
+            }
+            else if (go.Equals(GameObject.Find("Wrong")))
+            {
+                score += wp.addScore(-1);
+            }
+
+
+
+            GameObject.Find("Score").GetComponent<Text>().text = "Score: " + score;
+
+        }
+        if (!GameObject.Find("Panel").transform.GetChild(1).gameObject.activeInHierarchy)
+        {
+            GameObject.Find("Panel").transform.GetChild(1).gameObject.SetActive(true);
+            GameObject.Find("Panel").transform.GetChild(2).gameObject.SetActive(true);
+            GameObject.Find("Panel").transform.GetChild(3).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(4).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(5).gameObject.SetActive(false);
+        }
+
         bool randomPos = Random.Range(0, 2) == 0;
-        
-        Factory.WorldPower wp = Factory.Instance.generateAndRemove();
-        if (!wp.met) {
-            GameObject.Find("Correct").transform.GetChild(0).GetComponent<Text>().text = wp.a1R;
-            GameObject.Find("Wrong").transform.GetChild(0).GetComponent<Text>().text = wp.a1W;
-            GameObject.Find("Question").GetComponent<Text>().text = wp.q1;
-            
+        GameObject.Find("Question").GetComponent<Text>().fontSize = 60;
+        wp = Factory.Instance.generateAndRemove();
+        if (wp == null)
+        {
+            GameObject.Find("Panel").GetComponent<Image>().sprite = endImage;
+            GameObject.Find("Question").GetComponent<Text>().text = "The End";
+            GameObject.Find("Panel").transform.GetChild(1).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(2).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(3).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(4).gameObject.SetActive(false);
+            GameObject.Find("Panel").transform.GetChild(5).gameObject.SetActive(false);
         }
         else
         {
-            GameObject.Find("Correct").transform.GetChild(0).GetComponent<Text>().text = wp.a2R;
-            GameObject.Find("Wrong").transform.GetChild(0).GetComponent<Text>().text = wp.a2W;
-            GameObject.Find("Question").GetComponent<Text>().text = wp.q2;
-        }
 
-        if (this.gameObject.name.Equals("Correct"))
-        {
-            wp.addScore(1);
-        }
-        else
-        {
-            wp.addScore(-1);
-        }
 
-        if (randomPos)
-        {
-            GameObject.Find("Correct").transform.localPosition = new Vector3(200, 0, 0);
-            GameObject.Find("Wrong").transform.localPosition = new Vector3(-200, 0, 0);
-        }
+            if (!wp.met)
+            {
+                GameObject.Find("Correct").transform.GetChild(0).GetComponent<Text>().text = wp.a1R;
+                GameObject.Find("Wrong").transform.GetChild(0).GetComponent<Text>().text = wp.a1W;
+                GameObject.Find("Question").GetComponent<Text>().text = wp.q1;
+                GameObject.Find("Question").GetComponent<Text>().text = wp.leader + " of " + wp.name + " is asking: " + wp.q1;
+                if (wp.name.Equals("North Korea"))
+                {
+                    GameObject.Find("Wrong").SetActive(false);
+                    GameObject.Find("Correct").SetActive(false);
+                    GameObject.Find("Panel").transform.GetChild(3).gameObject.SetActive(true);
+                    GameObject.Find("Panel").transform.GetChild(4).gameObject.SetActive(true);
+                    GameObject.Find("Panel").transform.GetChild(5).gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                GameObject.Find("Correct").transform.GetChild(0).GetComponent<Text>().text = wp.a2R;
+                GameObject.Find("Wrong").transform.GetChild(0).GetComponent<Text>().text = wp.a2W;
+                GameObject.Find("Question").GetComponent<Text>().text = wp.leader + " of " + wp.name + " is asking: " + wp.q2;
+            }
 
-        else
-        {
-            GameObject.Find("Correct").transform.localPosition = new Vector3(-200, 0, 0);
-            GameObject.Find("Wrong").transform.localPosition = new Vector3(200, 0, 0);
+
+
+            if (randomPos && GameObject.Find("Panel").transform.GetChild(1).gameObject.activeInHierarchy)
+            {
+
+                GameObject.Find("Correct").transform.localPosition = new Vector3(200, -100, 0);
+                GameObject.Find("Wrong").transform.localPosition = new Vector3(-200, -100, 0);
+            }
+
+            else if (GameObject.Find("Panel").transform.GetChild(1).gameObject.activeInHierarchy)
+            {
+                GameObject.Find("Correct").transform.localPosition = new Vector3(-200, -100, 0);
+                GameObject.Find("Wrong").transform.localPosition = new Vector3(200, -100, 0);
+            }
+
+            GameObject.Find("Panel").GetComponent<Image>().sprite = wp.image;
+
+
+
         }
     }
 }
